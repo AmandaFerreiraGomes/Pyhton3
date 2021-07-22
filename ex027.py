705320907c523e0e79e56512c0f89d7d370c1a98
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-07-22 11:22:19 -0300

    Delete ex027.py

--- ex027.py
+++
@@ -1,9 +0,0 @@
-""" Desafio 027: """
-# Programa Principal:
-
-nome_completo = input('Digite seu nome completo: ')
-nome = nome_completo.split()
-x = len(nome) - 1
-print(f'Primeiro nome: {nome[0]}')
-print(f'Último nome: {nome[x]}')
-
