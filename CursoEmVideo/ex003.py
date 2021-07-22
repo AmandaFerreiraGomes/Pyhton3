f4559cb59f7acb343b29ecdb0cd68c19f6f8b1b6
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-07-22 11:07:03 -0300

    Update ex003.py

--- CursoEmVideo/ex003.py
+++ CursoEmVideo/ex003.py
@@ -1,9 +1,16 @@
 """ Desafio 003: """
 
+# Definição da função soma:
+
+def soma():
+    n1 = int(input('Digite um número: '))
+    n2 = int(input('Digite outro: '))
+    s = n1 + n2
+    
+    print(f'A soma entre {n1} e {n2} vale {s}.') # Usando o format de um jeito diferente do .format(...) 
+    print(f'{n1} + {n2} = {s}') # Por padrão, o tab tem de ser de 4 espaços.
+
 # Programa Principal:
 
-n1 = int(input('Digite um número: '))
-n2 = int(input('Digite outro: '))
-s = n1 + n2
-print(f'A soma entre {n1} e {n2} vale {s}.')
-print(f'{n1} + {n2} = {s}')
+if __name__ == "__main__":
+    soma()
