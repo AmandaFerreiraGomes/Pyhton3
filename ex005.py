67a31cfb55a3febae8932917af8955d276083fe1
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-07-22 11:07:27 -0300

    Delete ex005.py

--- ex005.py
+++
@@ -1,7 +0,0 @@
-""" Desafio 005: """
-
-n = int(input('Digite um número: '))
-a = n - 1
-s = n + 1
-print('O antecessor de {} é {} e seu sucessor é {}'.format(n, a, s))
-print(f'O antecessor de {n} é {n-1} e seu sucessor é {n + 1}')
