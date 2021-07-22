4a26dc20df882cd201ddc7cb3164f6267107872f
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-07-22 11:09:12 -0300

    Delete ex011.py

--- ex011.py
+++
@@ -1,7 +0,0 @@
-""" Desafio 011: """
-
-h = float(input('Altura: '))
-b = float(input('Largura: '))
-a = (b * h)
-tinta = (a / 2)
-print(f'Para pintar uma área de {a} m² serão necessários {tinta:.1f} litros de tinta.')
