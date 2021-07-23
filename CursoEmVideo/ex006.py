4d289f79f638023b677f7dca9e453b2dc3c3ec45
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-07-22 22:08:52 -0300

    Update ex006.py

--- CursoEmVideo/ex006.py
+++ CursoEmVideo/ex006.py
@@ -1,10 +1,19 @@
 """ Desafio 006: """
 
-print('-'*25)
-n = float(input('Digite um número: '))
-print('-'*25)
-raiz = n**(1/2)
-triplo = n * 3
-dobro = n * 2
-print(f'O dobro de {n} é {dobro}, o triplo de {n} é {triplo} e a raiz de {n} é {raiz:.2f}')
+def linha():
+    print('-'*25) # 25 caracteres - em sequência formando uma linha.
+
+# Programa Principal:
+
+if __name__ == "__main__":
+    
+    linha()
+    n = float(input('Digite um número: ')) # entrada de um número em forma de caractere o qual será convertido para real dentro da função float(). 
+    linha()
+    
+    raiz = n**(1/2) # n elevado a (1/2) => raiz quadrada de n.
+    triplo = n * 3 # n multiplicado por três => triplo de n.
+    dobro = n * 2 # n multiplicado por dois => dobro de n.
+    
+    print(f'O dobro de {n} é {dobro}, o triplo de {n} é {triplo} e a raiz de {n} é {raiz:.2f}')
 
