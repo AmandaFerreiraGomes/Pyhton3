1f38d0e3b09150b753ecf7d88951388e342bebca
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-07-22 22:21:29 -0300

    Update ex007.py

--- CursoEmVideo/ex007.py
+++ CursoEmVideo/ex007.py
@@ -1,10 +1,23 @@
 """ Desafio 007: """
 
-print('-'*25)
-nota1 = float(input('Digite a Nota 1: '))
-nota2 = float(input('Digite a Nota 2: '))
 
-med = ((nota1+nota2)/2)
-print('-'*25)
-print('Média: {:.1f}'.format( med))
-print('-'*25)
+def linha():
+    print('-'*25) # 25 caracteres '-' concatenados formando uma linha
+
+# Função que recebe dois parâmetros: nota1 e nota2 e retorna a média entre eles.
+
+def media(nota1, nota2):
+    med = ((nota1 + nota2)/2)
+    return med
+
+ # Programa Principal.
+
+if __name__ == "__main__":
+    
+    linha()
+    nota1 = float(input('Digite a Nota 1: ')) # nota 1 resultante da avaliação do tipo real
+    nota2 = float(input('Digite a Nota 2: ')) # nota 2 resultante da avaliação do tipo real
+    
+    linha()
+    print('Média: {:.1f}'.format( media(nota1, nota2)))
+    linha()
