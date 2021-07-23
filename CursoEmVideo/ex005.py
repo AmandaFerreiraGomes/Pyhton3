dec44706626cfa90102bc7e89db5acd34e241765
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-07-22 22:00:12 -0300

    Update ex005.py

--- CursoEmVideo/ex005.py
+++ CursoEmVideo/ex005.py
@@ -1,7 +1,15 @@
 """ Desafio 005: """
 
-n = int(input('Digite um número: '))
-a = n - 1
-s = n + 1
-print('O antecessor de {} é {} e seu sucessor é {}'.format(n, a, s))
-print(f'O antecessor de {n} é {n-1} e seu sucessor é {n + 1}')
+# Programa Principal:
+
+if __name__ == '__main__':
+    
+    n = int(input('Digite um número: ')) # solicita  a entrada de um número, o qual entra como caractere e é convertido para inteiro.
+    
+    a = n - 1 # antecessor de n
+    s = n + 1 # sucessor de n
+    # exemplo: para n = 10 => a = 9; s = 11.
+    
+    print('O antecessor de {} é {} e seu sucessor é {}'.format(n, a, s)) # método 1 de usar o format
+    
+    print(f'O antecessor de {n} é {n-1} e seu sucessor é {n + 1}') # método 2 de usar o format. *método de minha preferência, inclusive.
