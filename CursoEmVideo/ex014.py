058ab933a93be1c7f71a698f8254c91da18640b3
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-07-24 11:57:30 -0300

    Update ex014.py

--- CursoEmVideo/ex014.py
+++ CursoEmVideo/ex014.py
@@ -1,11 +1,24 @@
 """ Desafio 014: """
 
+
+def linha():
+    print('-'*45)
+
+
 # Programa Principal:
-print('-'*45)
-print('	  CONVERSÃO DE TEMPERATURA: ºC -> ºF.')
-print('-'*45)
-c = float(input('Digite a temperatura em graus Celsius(ºC): '))
-f = ((9*c + 160)/5)
-print('-'*45)
-print(f'	-> {c:.1f}ºC correspondem a {f:.1f}ºF.')
-print('-'*45)
+if __name__ == "__main__":
+    # cabeçalho:
+    linha()
+    print('	  CONVERSÃO DE TEMPERATURA: ºC -> ºF.')
+    linha()
+    
+    # entrada do valor da temperatura em graus celsius:
+    c = float(input('Digite a temperatura em graus Celsius(ºC): '))
+    
+    # cálculo de conversão de celsius para fahrenheit:
+    f = ((9*c + 160)/5)
+    
+    # exibição do resultado da conversão:
+    linha()
+    print(f'	-> {c:.1f}ºC correspondem a {f:.1f}ºF.')
+    linha()
