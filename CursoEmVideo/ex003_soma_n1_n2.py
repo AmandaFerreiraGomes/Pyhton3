55a353bca5818af38d3580f3dc9cb6240f9fc213
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-07-24 11:19:19 -0300

    ajustando alguns detalhes

--- CursoEmVideo/ex003_soma_n1_n2.py
+++ CursoEmVideo/ex003_soma_n1_n2.py
@@ -2,15 +2,21 @@
 
 # Definição da função soma:
 
-def soma():
-    n1 = int(input('Digite um número: '))
-    n2 = int(input('Digite outro: '))
+def soma(n1, n2):
     s = n1 + n2
     
-    print(f'A soma entre {n1} e {n2} vale {s}.') # Usando o format de um jeito diferente do .format(...) 
-    print(f'{n1} + {n2} = {s}') # Por padrão, o tab tem de ser de 4 espaços.
 
 # Programa Principal:
 
 if __name__ == "__main__":
-    soma()
+    
+    # entrada dos valores n1 e n2
+    n1 = int(input('Digite um número: '))
+    n2 = int(input('Digite outro: '))
+    
+    # chamada da função soma, a qual recebe os parâmetros n1 e n2
+    soma(n1, n2)
+    
+    # exibição do resultado da operação soma
+    print(f'A soma entre {n1} e {n2} vale {s}.') # Usando o format de um jeito diferente do .format(...) 
+    print(f'{n1} + {n2} = {s}') # Por padrão, o tab tem de ser de 4 espaços.
