b7f8111d7bf731b68c3726cc693f7c29d58ecf29
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-07-24 11:27:09 -0300

    Update and rename ex006.py to ex006_raiz_triplo_dobro.py

--- CursoEmVideo/ex006_raiz_triplo_dobro.py
+++ CursoEmVideo/ex006_raiz_triplo_dobro.py
@@ -15,5 +15,6 @@ if __name__ == "__main__":
     triplo = n * 3 # n multiplicado por três => triplo de n.
     dobro = n * 2 # n multiplicado por dois => dobro de n.
     
+    # exibição do resultado das operações:
     print(f'3 - O dobro de {n} é {dobro}\n2 - o triplo de {n} é {triplo}\n1 - a raiz de {n} é {raiz:.2f}\n')
 
