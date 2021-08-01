4275caa7f9ca5dbaa05d1ffa277e8e3e01e99076
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-08-01 11:39:45 -0300

    Update ex045_pedra_papel_tesoura.py

--- CursoEmVideo/ex045_pedra_papel_tesoura.py
+++ CursoEmVideo/ex045_pedra_papel_tesoura.py
@@ -6,22 +6,46 @@ import random
 
 
 def linha():
-	print()
+	print('\033[34m', end='')
+	print('-'*33)
+	print('\033[m', end='')
+
+def joquempo(choice_user, nome):
+	L = ['PEDRA', 'PAPEL', 'TESOURA']  # tupla com as opcoes de jogo.
+	
+	choice_computer = random.choice(L)
+	
+	print(f'Computador: {choice_computer}'.upper())
+	linha()
+	
+	if choice_computer == choice_user:
+		print('Empate.'.upper())
+	elif (choice_computer == 'PEDRA' and choice_user == 'PAPEL') or (choice_computer == 'TESOURA' and choice_user == 'PEDRA') \
+			or (choice_computer == 'PAPEL' and choice_user == 'TESOURA'):
+		print(f'{nome} Venceu!'.upper())
+	elif (choice_computer == 'PAPEL' and choice_user == 'PEDRA') or (choice_computer == 'PEDRA' and choice_user == 'TESOURA') \
+			or (choice_computer == 'TESOURA' and choice_user == 'PAPEL'):
+		print('Computador Venceu!'.upper())
 
 
 # Programa Principal:
-def main():
-	nome = str(input('Digite seu nome: '))
-	L = ['Pedra', 'Papel', 'Tesoura']
-	choice_computer = random.choice(L)
-	choice_user = str(input('Digite Pedra, Papel ou Tesoura.'))
-	print(f'Computador: {choice_computer}')
-	if choice_computer == choice_user:
-		print('Empate.')
-	elif (choice_computer == 'Pedra' and choice_user == 'Papel') or (choice_computer == 'Tesoura' and choice_user == 'Pedra') or (choice_computer == 'Papel' and choice_user == 'Tesoura'):
-		print(f'{nome} Venceu!')
-	elif (choice_computer == 'Papel' and choice_user == 'Pedra') or (choice_computer == 'Pedra' and choice_user == 'Tesoura') or (choice_computer == 'Tesoura' and choice_user == 'Papel'):
-		print('Computador Venceu!')
-
+if __name__ == '__main__':
 	
-main()
+	linha()
+	print('\033[31m--------- JOQUEMPO v2.0.---------\033[m')
+	linha()
+	
+	print('\033[36m', end='')
+	nome = input('Digite seu nome: '.upper())
+	print('\033[m', end='')
+	
+	linha()
+	print('\033[34m', end='')
+	choice_user = input('Digite Pedra, Papel ou Tesoura: '.upper()).upper()
+	print('\033[m', end='')
+	linha()
+
+	joquempo(choice_user, nome)
+	linha()
+
+
