b39bb82dbd4c4bcd368ccce32891f9433eee15f2
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-08-01 10:42:20 -0300

    Update ex044.py

--- CursoEmVideo/ex044.py
+++ CursoEmVideo/ex044.py
@@ -1,33 +1,88 @@
 """ Desafio 044: """
-# Programa Principal:
 
 
-def main():
-	preco = float(input('Digite o preço do produto: R$ '))
-	condicao = int(input("""Condição de Pagamento:
-	1 - À VISTA(DINHEIRO OU CHEQUE).
-	2 - À VISTA NO CARTÃO.
-	3 - DUAS PARCELAS NO CARTÃO.
-	4 - TRÊS PARCELAS OU MAIS."""))
+def menu():
+	
+	print("""\t\tCondição de Pagamento:
+	
+1 - À VISTA(DINHEIRO OU CHEQUE).
+2 - À VISTA NO CARTÃO.
+3 - DUAS PARCELAS NO CARTÃO.
+4 - TRÊS PARCELAS OU MAIS.
+""".upper(), end='')
+
+
+def linha():
+	
+	print('-'*36)
+
+def condicao():
+	
+	cond = int(input('Opção: '.upper()))
+	return cond
+
+
+def pagamento(cond, preco):
+	
+	condicao = cond
+	pag = 0 # valor cobrado no final
+	desconto = 0 # desconto sobre o preco do produto
+	acrescimo = 0
+	vez = 0
+	
 	if condicao == 1:
-		pagamento = (preco - (preco*0.1))
-		print(f'Preço: R$ {pagamento}.\nCom desconto de 10%.')
+		desconto = 10
+		pag = (preco - (preco * (desconto/100)))
+		vez = 1
 	elif condicao == 2:
-		pagamento = (preco - (preco*0.05))
-		print(f'Preço: R$ {pagamento}.\nCom desconto de 5%')
+		desconto = 5
+		pag = (preco - (preco * (desconto/100)))
+		vez = 1
+
 	elif condicao == 3:
-		pagamento = preco / 2
-		print(f'2 x de R$ {pagamento}.')
+		pag = preco / 2
+		desconto = 0
+		vez = 2
+		
 	elif condicao == 4:
-		vezes = int(input('Quantidade de Parcelas: '))
-		if vezes>2:
-			pagamento = ((preco + (preco*0.2))/vezes)
-			print(f'{vezes} x de R$ {pagamento}')
+		
+		vezes = int(input('Quantidade de Parcelas: '.upper()))
+		
+		if vezes > 2:
+			acrescimo = 20
+			paga = ((preco + (preco * (acrescimo/100))) / vezes)
+			vez = vezes
+		
+		
 		else:
 			print('Escolha a Condição 3.')
-		
+	
 	else:
 		print('Opção Inválida!')
-	
+		
+	print(f'Preço: {vez} x de R$ {pag:.2f}.\nCom desconto de {desconto}%\nCom acréscimo de {acrescimo}%'.upper())
 
-main()
+
+# Programa Principal:
+if __name__ == "__main__":
+	
+	try:
+		linha()
+		print(' BEM-VINDO AO SISTEMA DESCONTO V2.0 ')
+		linha()
+		
+		preco = float(input('-> Digite o preço do produto: R$ '.upper()))
+		linha()
+		
+		menu()
+		linha()
+		
+		cond = condicao()
+		linha()
+		
+		pagamento(cond, preco)
+		linha()
+	
+	except (KeyboardInterrupt, KeyError, ValueError):
+		print("\033[31mERRO NA EXECUÇÃO DO PROGRAMA!")
+	
