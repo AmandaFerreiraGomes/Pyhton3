48f75aed5a8f3277ffcdfcac824dcec3ad92d5f2
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-08-01 15:42:54 -0300

    Update ex046_contagem_regressiva.py

--- CursoEmVideo/ex046_contagem_regressiva.py
+++ CursoEmVideo/ex046_contagem_regressiva.py
@@ -1,17 +1,25 @@
 """ Desafio 046: """
-# Programa Principal:
-import time, emoji
+
+from time import sleep 
+from emoji import emojize
 
 
 def contagem(): # contagem regressiva para a virada do ano (são dez segudos).
 	for e in range(10, 0, -1):
 		print(f'{e}')
-		time.sleep(1)
+		sleep(1) # utiliza-se a função sleep importada da biblioteca time para fazer a contagem de diminuição de ums segundo por loop do for.
 
+
+def linha():
+	print('-'*48)
 
 if __name__ == "__main__":
 	
-	contagem()
-	print(4*emoji.emojize(':fireworks:')) # exibe 4 emojis de fogos de artifício.
-
-	
+	try:
+		linha()
+		print('--- CONTAGEM REGRESSIVA PARA A VIRADA DO ANO ---')
+		linha()
+		contagem()
+		print(4*emojize(':fireworks:'))
+	except (KeyboardInterrupt, ValueError, KeyError):
+		print('\033[31mErro!\033[m')
