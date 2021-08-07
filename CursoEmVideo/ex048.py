25fcf0a132ed98ab7a42afed6e5a2b66f4eca423
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-08-07 18:32:27 -0300

    Update ex048.py

--- CursoEmVideo/ex048.py
+++ CursoEmVideo/ex048.py
@@ -1,18 +1,35 @@
 """ Desafio 048: """
 import emoji
+
+
+def linha():
+	print('-'*66)
+
+
 # Programa Principal:
-
-
 def main():
-	print('\033[34mÍmpares múltiplos de 3 que se encontram no intervalo [1, 500].\033[m')
+	
+	# cabecalho.
+	linha()
+	print('\033[34m  Ímpares múltiplos de 3 que se encontram no intervalo [1, 500].  \033[m')
+	linha()
+	
+	# inicializa-se a variável soma.
 	soma = 0
-	for e in range(1, 500):
+	
+	for e in range(1, 501):
 		if (e % 2 != 0) and (e % 3 == 0):
 			impar = e
 			soma = soma + e
 			print('->', impar)
+	
+	# resultado da soma de todos os valores ímpares múltiplos de 3, os quais se encontram no intervalo fechado [1, 500].
 	print(f'Soma: {soma}')
+	
+	# mostra um emoji de óculos de sol.
 	print(emoji.emojize(':sunglasses:'))
 
-	
-main()
+
+if __name__ == '__main__':
+	main()
+
