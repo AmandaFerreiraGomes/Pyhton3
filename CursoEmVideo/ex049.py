70c06c28e50c850d8cf07307dc34c657b298b2ff
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-08-07 22:09:44 -0300

    Update ex049.py

--- CursoEmVideo/ex049.py
+++ CursoEmVideo/ex049.py
@@ -2,11 +2,29 @@
 # Programa Principal:
 
 
+def linha():
+	print('-'*34)
+
+
 def main():
+	
+	# inserção de dados pelo usuário
+	linha()
 	n = int(input('Digite um número: '))
-	print(f'----------Tabuada de {n}----------')
+	
+	# cabeçalho.
+	linha()
+	print(f'---------- Tabuada de {n} ----------')
+	linha()
+	
+	# laço que calcula e exibe o resultado da tabela de soma de 'n'.
+	# para e no intervalo [0, 10], com e iniciando com zero.
 	for e in range(0, 11):
 		print(f'{n} + {e} = {n + e}')
+	
+	linha()
 
 
-main()
+if __name__ == '__main__':
+	main()
+
