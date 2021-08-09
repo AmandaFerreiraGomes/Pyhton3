7d9b2f76b6eb12dbc319bf59587a7674429d7ea3
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-08-08 22:52:49 -0300

    Update ex060.py

--- CursoEmVideo/ex060.py
+++ CursoEmVideo/ex060.py
@@ -1,14 +1,26 @@
 """ Desafio 060: """
+
+
 import math
+
+
+def linha():
+	print('-' * 30)
+
+	
 # Programa Principal:
-
-
 def main():
+	
 	print('\033[36m-=\033[m'*25)
+	
 	f = int(input('Digite um número: '))
+	
 	c = f
+	
 	fat = math.factorial(f)
+	
 	print(f'-=-=-=-=-=-= FATORIAL DE {f}! -=-=-=-=-=-=')
+	
 	while c >= 1:
 		if c == 1:
 			print(f' {c} = {fat}')
@@ -17,4 +29,5 @@ def main():
 		c = c - 1
 		
 	
-main()
+if __name__ == '__main__':
+	main()
