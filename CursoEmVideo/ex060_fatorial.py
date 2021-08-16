6481559554b290bdb53d1ea328d2ed5224db6175
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2021-08-16 09:03:14 -0300

    Update ex060_fatorial.py

--- CursoEmVideo/ex060_fatorial.py
+++ CursoEmVideo/ex060_fatorial.py
@@ -11,21 +11,27 @@ def linha():
 # Programa Principal:
 def main():
 	
-	print('\033[36m-=\033[m'*25)
-	
+	# entrada de dados.
+	linha()
 	f = int(input('Digite um número: '))
 	
 	c = f
-	
 	fat = factorial(f)
 	
-	print(f'-=-=-=-=-=-= FATORIAL DE {f}! -=-=-=-=-=-=')
+	# cabeçalho do resultado.
+	linha()
+	print(f' FATORIAL DE {f}! '.center(30))
+	linha()
 	
+	# enquanto o valor digitado for maior que 1
 	while c >= 1:
+		# se c for igual a 1, então imprima c e o fatorial de c.
 		if c == 1:
 			print(f' {c} = {fat}')
+		# caso contrário imprima 'c x' sem quebra de linha.
 		else:
 			print(f' {c} x', end='')
+		# c é decrementado a cada nova repetição.
 		c = c - 1
 		
 	
