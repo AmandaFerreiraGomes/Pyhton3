ee60f8c5835338089eadb1fbfc0018c58412a8a0
Amanda Gomes <50182271+AmandaFerreiraGomes@users.noreply.github.com>
2022-03-17 16:50:25 -0300

    Update ex002_apresentacao.py
    Adicionando Type Hints.

--- CursoEmVideo/ex002_apresentacao.py
+++ CursoEmVideo/ex002_apresentacao.py
@@ -2,12 +2,12 @@
 """ Desafio 002: """
 
 
-def azul():
-    cores = {
+def azul() -> str :
+    cores : dict = {
         'azul': '\033[34m'
     }
     return cores['azul']
 
 if __name__ == '__main__':
-    a = input('Digite seu nome: ')
+    a : str = input('Digite seu nome: ')
     print(f'É um prazer te conhecer, {azul()}{a}!')
